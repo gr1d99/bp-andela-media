5782ad8e6155039f5c671675fdd29e32040fb4c6
Gideon Kimutai <gideonkimutai9@gmail.com>
2019-02-04 12:21:52 +0300

    use specify instead of it

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -2,11 +2,13 @@ require "rails_helper"
 
 RSpec.describe Role, type: :model do
   let!(:role) { create :role }
-  it do
+
+  specify do
     should validate_presence_of(:name).
       with_message(I18n.t("errors.role.blank"))
   end
-  it do
+
+  specify do
     should validate_uniqueness_of(:name).
       with_message(I18n.t("errors.role.unique"))
   end
