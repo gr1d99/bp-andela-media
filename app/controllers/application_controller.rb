5fa564d5ea9eff9cf3dfbd6b9cec8c0b357c6a4f
Gideon Kimutai <gideonkimutai9@gmail.com>
2019-02-05 08:34:24 +0300

    remove authentication and authorization logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::API
   before_action :set_locale
+  include AuthenticationConcern
   include Response
   include ExceptionHandler
 
@@ -11,40 +12,5 @@ class ApplicationController < ActionController::API
 
   def default_url_options
     { locale: I18n.locale }
-  end
-
-  def authenticate
-    token = get_token
-    decoded_token = validate_token(token)
-    return unauthorized unless decoded_token
-
-    user = User.find_by(id: decoded_token["UserInfo"]["id"])
-    return head :forbidden unless user && admin?(user)
-
-    @current_user = user
-  end
-
-  def unauthorized
-    head :unauthorized
-  end
-
-  def admin?(user)
-    user.role.name == "Admin"
-  end
-
-  def get_token
-    return nil unless request.headers["Authorization"]
-
-    request.headers["Authorization"].split(" ")[1]
-  end
-
-  def validate_token(token)
-    JWT.decode(*jwt_params(token)).first
-  rescue JWT::VerificationError, JWT::IncorrectAlgorithm, JWT::DecodeError
-    false
-  end
-
-  def jwt_params(token)
-    [token, nil, false]
   end
 end
